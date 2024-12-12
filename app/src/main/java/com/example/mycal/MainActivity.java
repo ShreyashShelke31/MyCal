358dac9de79e6ecde6e19cb24ace10b8458f503c
ShreyashShelke31 <shelkeshreyash31@gmail.com>
2024-12-12 15:42:30 +0530

    First Commit

---
+++ app/src/main/java/com/example/mycal/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.mycal;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etNum1, etNum2;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etNum1 = findViewById(R.id.et_num1);
+        etNum2 = findViewById(R.id.et_num2);
+        tvResult = findViewById(R.id.tv_result);
+
+        Button btnAdd = findViewById(R.id.btn_add);
+        Button btnSubtract = findViewById(R.id.btn_subtract);
+        Button btnMultiply = findViewById(R.id.btn_multiply);
+        Button btnDivide = findViewById(R.id.btn_divide);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('+');
+            }
+        });
+
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('-');
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('*');
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('/');
+            }
+        });
+    }
+
+    private void calculate(char operator) {
+        String num1Str = etNum1.getText().toString();
+        String num2Str = etNum2.getText().toString();
+
+        if (num1Str.isEmpty() || num2Str.isEmpty()) {
+            tvResult.setText("Invalid Ibput");
+            return;
+        }
+
+        double num1 = Double.parseDouble(num1Str);
+        double num2 = Double.parseDouble(num2Str);
+        double result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                tvResult.setText("Cannot divide by zero");
+                return;
+            }
+        }
+
+        tvResult.setText("Result: " + result);
+    }
+}
